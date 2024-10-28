cdf7981aca5e5d696ee5f51c8701ca09b5fa3e19
James <jamesrishton03@gmail.com>
2024-10-28 13:10:51 +0000

    Implementing parametrization

--- test_times.py
+++ test_times.py
@@ -1,31 +1,16 @@
 from times import time_range, compute_overlap_time
 from pytest import raises
-
-def test_generic_case():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-    assert compute_overlap_time(large, short) == [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-
-def test_more_intervals_in_large():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2)
-    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00")
-    assert compute_overlap_time(large, short) == [('2010-01-12 10:30:00', '2010-01-12 10:45:00')]
-
-def test_no_overlap():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
-    assert compute_overlap_time(large, short) == []
+import pytest
 
 def test_negative_time_range():
     with raises(ValueError):
         time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
-        
 
-def test_edges():
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 12:00:00", "2010-01-12 14:00:00")
-    assert compute_overlap_time(large, short) == []
 
-    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-    short = time_range("2010-01-12 08:00:00", "2010-01-12 10:00:00")
-    assert compute_overlap_time(large, short) == []
+@pytest.mark.parametrize("range1, range2, expected", [(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]),
+                                            (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2), time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00"), [('2010-01-12 10:30:00', '2010-01-12 10:45:00')]),
+                                            (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00"), []),
+                                            (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 12:00:00", "2010-01-12 14:00:00"), []),
+                                            (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"), time_range("2010-01-12 08:00:00", "2010-01-12 10:00:00"), [])])
+def test_various_time_overlaps(range1, range2, expected):
+    assert compute_overlap_time(range1, range2) == expected
